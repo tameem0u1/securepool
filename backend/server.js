3621c06741c85405f2db6251ccf5df94c4594b30
Kenneth 'Kenny' Heagle <KennethH@mjcpa.com>
2025-07-17 16:38:51 -0600

    merge

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors());
 app.use(json());
 
+const database_name = "securepool_db";
+
 // Connect to your local MySQL server
 
 const db = await initializeDatabase();
